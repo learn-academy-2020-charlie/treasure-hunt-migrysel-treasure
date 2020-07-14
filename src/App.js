75d002e5fe2c3948fb58143285a177f07f64f0d9
selamhabteab <selamhabteab@gmail.com>
2020-07-14 14:28:53 -0700

    1-2 user tasks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Square from './components/Square.js'
 import './App.css'
 
 class App extends Component{
@@ -6,13 +7,30 @@ class App extends Component{
     super(props)
     this.state = {
       squares: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
+
     }
   }
 
+handleLocation = (indexLocation) => {
+  alert (indexLocation)
+}
+
   render(){
+    let box = this.state.squares.map((value, index)=> {
+      return (
+        <Square
+        value = {value}
+        index = {index}
+        handleLocation = {this.handleLocation}
+        />
+      )
+    })
     return(
       <React.Fragment>
-        <h1>Treasure Hunt App</h1>
+      <h1>Treasure Hunt App</h1>
+      <div id="board">
+        { box }
+      </div>
       </React.Fragment>
     )
   }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
 
+
 class Square extends Component{
+
+  cursorClick = ()=>{
+    this.props.handleLocation(this.props.index)
+  }
+
   render(){
     return(
       <React.Fragment>
+        <div id="square" onClick = {this.cursorClick}>
+        {this.props.value}
+        </div>
       </React.Fragment>
     )
   }
